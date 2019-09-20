3f9be932900a4e26f66faedd50fb92e43227852f
Oscar <oscar870524@gmail.com>
2019-09-20 12:15:00 +0800

    新增：Keyboard Hook 但功能有缺陷

--- Hook_Test/Keyboard_Hook_Test.xaml.cs
+++ Hook_Test/Keyboard_Hook_Test.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Hook_Test.Model;
+
 namespace Hook_Test
 {
     /// <summary>
@@ -23,5 +25,69 @@ namespace Hook_Test
         {
             InitializeComponent();
         }
+
+        Keyboard_Hook keyboard_hook = new Keyboard_Hook();
+
+        private void hook_KeyDown(object sender, Key key)
+        {
+            display.Text += "press " + key + Environment.NewLine;
+        }
+
+        private void set_hook_event()
+        {
+            keyboard_hook.KeyDownEvent += hook_KeyDown;
+        }
+
+        private void set_local_hook(object sender, RoutedEventArgs e)
+        {
+           
+            if (keyboard_hook.keyboard_handler == 0)
+            {
+                set_hook_event();
+                display.Text += "設置 Local Hook " + (keyboard_hook.Start() ? "成功" : "失敗") + "!\r\n";
+            }
+            else
+            {
+                keyboard_hook.Stop();
+            }
+            local_hook_btn.Content = keyboard_hook.keyboard_handler == 0 ? "設置 Local Hook" : "移除 Global Hook";
+            global_hook_btn.IsEnabled = keyboard_hook.keyboard_handler == 0 ? true : false;
+        }
+
+        private void set_global_hook(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (keyboard_hook.keyboard_handler == 0)
+            {
+                set_hook_event();
+                display.Text += "設置 Global Hook " + (keyboard_hook.Start(true) ? "成功" : "失敗") + "!\r\n";               
+            }
+            else
+            {
+                display.Text += "移除 Global Hook " + (keyboard_hook.Stop() ? "成功" : "失敗") + "!\r\n";
+            }
+            global_hook_btn.Content = keyboard_hook.keyboard_handler == 0 ? "設置 Global Hook" : "移除 Global Hook";
+            local_hook_btn.IsEnabled = keyboard_hook.keyboard_handler == 0 ? true :false ;
+        }
+
+        private void handle_keyboard(object sender, RoutedEventArgs e) {
+            keyboard_hook.keyboard_hook_handle = !keyboard_hook.keyboard_hook_handle;
+            Button btn = (Button)sender;
+            btn.Content = keyboard_hook.keyboard_hook_handle ? "取消 攔截不輸入鍵盤" : "設定 攔截不輸入鍵盤";
+
+        }
+
+        private void display_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            display.ScrollToEnd();
+        }
+
+        private void display_clear(object sender, RoutedEventArgs e) {
+            display.Clear();
+        }
+
+
+
+
     }
 }
---
+++ Hook_Test/Model/Hook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Hook_Test.Model
+{
+    class Hook
+    {
+        protected delegate int HookProc(int nCode, Int32 wParam, IntPtr lParam);
+
+        //使用此功能，安裝了一個鉤子
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        protected static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+
+        //調用此函數卸載鉤子
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        protected static extern bool UnhookWindowsHookEx(int idHook);
+
+
+        //使用此功能，通過信息鉤子繼續下一個鉤子
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        protected static extern int CallNextHookEx(int idHook, int nCode, Int32 wParam, IntPtr lParam);
+
+        // 取得當前線程編號（線程鉤子需要用到）
+        [DllImport("kernel32.dll")]
+        protected static extern int GetCurrentThreadId();
+
+        //使用WINDOWS API函數代替獲取當前實例的函數,防止鉤子失效
+        [DllImport("kernel32.dll")]
+        protected static extern IntPtr GetModuleHandle(string name);
+
+    }
+}
