71be9f4fe16c1bc5211ccb108cf681356f589b2f
Oscar <oscar870524@gmail.com>
2019-09-20 09:44:21 +0800

    git init

---
+++ Hook_Test/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hook_Test
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void page_keyboard_hook_test(object sender, RoutedEventArgs e)
+        {
+            page_view.Children.Clear();
+            page_view.Children.Add(new Keyboard_Hook_Test());
+        }
+    }
+}
